feb9b9eb628ed644151dc4fa553916a277729c47
Erik Galindo <erikgalindo40@yahoo.com>
2023-03-23 13:24:55 -0700

    add modal for recipes

--- frontend/src/components/Recipe.jsx
+++ frontend/src/components/Recipe.jsx
@@ -1,19 +1,28 @@
-function Recipe({ recipeName, time, description, directions, ingredients, recipeID }) {
+import { HiOutlineDotsHorizontal } from 'react-icons/hi'
+
+function Recipe({ recipeName, time, description, directions, ingredients, recipeID, setEditRecipeModal, editRecipeModal }) {
     const parsedIngredients = JSON.parse(ingredients)
     const parsedDirections = JSON.parse(directions)
 
     return (
     <li className="recipe">
+        {editRecipeModal&& <div className='recipe-modal'>
+            <button className='recipe-modal-edit-button modal-button'>EDIT</button>
+            <button className='recipe-modal-delete-button modal-button'>DELETE</button>
+        </div>}
         <div className="recipe-header">
-            <h2 className='recipe-name'>{recipeName}</h2>
-            <p className='recipe-time'>{time}</p>
+            <div>
+                <h2 className='recipe-name'>{recipeName}</h2>
+                <p className='recipe-time'>{time}</p>
+            </div>
+            <div onClick={()=>setEditRecipeModal(!editRecipeModal)} className='recipe-edit-button'><HiOutlineDotsHorizontal/></div>
         </div>
         <p className='recipe-description'>{description}</p>
         <h3>Ingredients</h3>
         <ul className="recipe-ingredients">
             {parsedIngredients.map(ingredient=>(
                 <li key={ingredient.id} className="recipe-ingredient">
-                    <span className='ingredient-amount'>{ingredient.amount}</span>
+                    <span className='ingredient-amount'>- {ingredient.amount}</span>
                     <span className='ingredient-unit'> {ingredient.unit} </span>
                     <span className='ingredient-name'>{ingredient.name}</span>
                 </li>
