a965d854b008d9f23e9b610ef962efc0c7853e0c
Erik Galindo <erikgalindo40@yahoo.com>
2023-03-23 12:12:57 -0700

    edit recipe styles

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import RecipeForm from '../components/RecipeForm'
 import Recipe from '../components/Recipe'
+import { HiOutlineDotsHorizontal } from 'react-icons/hi'
 
 function Dashboard() {
   const navigate = useNavigate()
@@ -43,6 +44,46 @@ function Dashboard() {
           {recipeList.map(recipe=>(
             <Recipe key={recipe._id} recipeID={recipe._id} recipeName={recipe.name} ingredients={recipe.ingredients} directions={recipe.directions} time={recipe.time} description={recipe.description}/>
           ))}
+        
+        <li key={'lmao'} className="recipe">
+        <div className="recipe-header">
+          <div>
+            <h2 className='recipe-name'>A super long really long recipe name</h2>
+            <p className='recipe-time'>10min</p>
+          </div>
+            <div className='recipe-edit-button'><HiOutlineDotsHorizontal/></div>
+        </div>
+        <p className='recipe-description'>This is a description of the meal lol</p>
+        <h3>Ingredients</h3>
+        <ul className="recipe-ingredients">
+                <li key={'lol'} className="recipe-ingredient">
+                    <span className='ingredient-amount'>- 2</span>
+                    <span className='ingredient-unit'> cup/s </span>
+                    <span className='ingredient-name'>Milk</span>
+                </li>
+                <li key={'lol2'} className="recipe-ingredient">
+                    <span className='ingredient-amount'>- 2</span>
+                    <span className='ingredient-unit'> cup/s </span>
+                    <span className='ingredient-name'>Milk</span>
+                </li>
+                <li key={'lol3'} className="recipe-ingredient">
+                    <span className='ingredient-amount'>- 2</span>
+                    <span className='ingredient-unit'> cup/s </span>
+                    <span className='ingredient-name'>Milk</span>
+                </li>
+                <li key={'lol4'} className="recipe-ingredient">
+                    <span className='ingredient-amount'>- 2</span>
+                    <span className='ingredient-unit'> cup/s </span>
+                    <span className='ingredient-name'>Milk</span>
+                </li>
+        </ul>
+        <ol className='recipe-directions'>
+            <h3>Directions</h3>
+                <li key={'huh'} className="recipe-direction">
+                    This is step number one and also the final step
+                </li>
+        </ol>
+    </li>
         </ul>
     </main>
   )
